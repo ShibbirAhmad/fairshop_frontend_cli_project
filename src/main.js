0064c0e487fb5936565e4f992afeb0d7ef4ef066
Imran Hossain <imranhossainppip@gmail.com>
2022-04-07 16:42:21 +0600

    fix: mobile view add to cart and buy now button

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ Vue.mixin(helper);
 
 
 //Vue.use(axios)
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api/';
+axios.defaults.baseURL = 'https://app.fairshop.com.bd/api/';
 
 
 Vue.prototype.$axios = axios;
